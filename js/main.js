4e09df66efd6b519d1785060ce4f4e2329682f31
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2022-07-20 23:28:40 +0200

    update

--- js/main.js
+++ js/main.js
@@ -11,3 +11,24 @@
       );
   }
 });
+
+// Auto Text Typing
+const textEl = document.getElementById('text');
+const text = '“There is only one you on the globe today. \n You have been built to inspire and designed to dazzle. \n Live your uniqueness!”'
+
+let idx = 1;
+let speed = 150;
+
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, idx);
+
+  idx++
+
+  if(idx > text.length) { 
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
